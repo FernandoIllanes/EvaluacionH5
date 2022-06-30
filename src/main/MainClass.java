2c567e68378cf4e8036067550879aac9816afb09
FernandoIllanes <lpze.alvarofernando.illanes.ca@unifranz.edu.bo>
2022-06-30 00:32:50 -0400

    version 0.5

---
+++ src/main/MainClass.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.swing.JOptionPane;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		new Game();
+	}
+
+}
